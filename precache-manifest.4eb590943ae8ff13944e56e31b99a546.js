74e89010da87211172e90f0f81e051f9a1605ad5
AbhishekSatyam96 <abhishek.satyam96@gmail.com>
2019-12-06 21:56:01 +0530

    Updates

---
+++ precache-manifest.4eb590943ae8ff13944e56e31b99a546.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd956d678deaecdcc4ee04436a2940c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a33a20c1a84064e8f24",
+    "url": "/static/css/main.985657b6.chunk.css"
+  },
+  {
+    "revision": "05bb7b9866f85a6fdddf",
+    "url": "/static/js/2.41100649.chunk.js"
+  },
+  {
+    "revision": "4a33a20c1a84064e8f24",
+    "url": "/static/js/main.0de210d5.chunk.js"
+  },
+  {
+    "revision": "a695b6c475ac36abad8b",
+    "url": "/static/js/runtime-main.0ebeed79.js"
+  }
+]);
